e4667b042c1f9d15f848ce6b833d40e84c51b654
tuoitho <tuoithokhoc1414@gmail.com>
2024-11-28 16:11:27 +0700

    delete cái đã bỏ

--- src/main/java/com/cosmeticsellingwebsite/config/CaptchaSettings.java
+++ src/main/java/com/cosmeticsellingwebsite/config/CaptchaSettings.java
@@ -1,4 +1,4 @@
-package com.cosmeticsellingwebsite.setting;
+package com.cosmeticsellingwebsite.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
--- src/main/java/com/cosmeticsellingwebsite/service/capcha/CaptchaService.java
+++ src/main/java/com/cosmeticsellingwebsite/service/capcha/CaptchaService.java
@@ -2,7 +2,7 @@ package com.cosmeticsellingwebsite.service.capcha;
 
 
 import com.cosmeticsellingwebsite.dto.gooogle.GoogleResponse;
-import com.cosmeticsellingwebsite.setting.CaptchaSettings;
+import com.cosmeticsellingwebsite.config.CaptchaSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
