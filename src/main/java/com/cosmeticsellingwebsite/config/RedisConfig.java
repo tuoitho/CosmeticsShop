87a3acc5d557e0213860d3d182946f2159ba48ce
tuoitho <tuoithokhoc1414@gmail.com>
2024-11-29 23:50:32 +0700

    BỔ SUNG REDIS for storing OTP and caching dât

--- src/main/java/com/cosmeticsellingwebsite/config/RedisConfig.java
+++ src/main/java/com/cosmeticsellingwebsite/config/RedisConfig.java
@@ -1,38 +1,38 @@
-//package com.cosmeticsellingwebsite.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//
-//@Configuration
-//public class RedisConfig {
-//    @Value("${redis.host}")
-//    private String redisHost;
-//
-//    @Value("${redis.port}")
-//    private int redisPort;
-//
-//
-//    @Bean
-//    public LettuceConnectionFactory redisConnectionFactory() {
-//        // Tạo Standalone Connection tới Redis
-//        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
-//    }
-//
-//    @Bean
-//    @Primary
-//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        // tạo ra một RedisTemplate
-//        // Với Key là Object
-//        // Value là Object
-//        // RedisTemplate giúp chúng ta thao tác với Redis
-//        RedisTemplate<Object, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(redisConnectionFactory);
-//        return template;
-//    }
-//}
+package com.cosmeticsellingwebsite.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${redis.host}")
+    private String redisHost;
+    @Value("${redis.port}")
+    private int redisPort;
+    @Value("${redis.password}")
+    private String redisPassword;
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+        configuration.setPassword(redisPassword);
+        return new JedisConnectionFactory(configuration);
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+}
