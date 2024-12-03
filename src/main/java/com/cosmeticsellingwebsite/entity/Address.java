9a6781b96b2c5b0ed8bdf3b3b0ecc93816f02f88
tuoitho <tuoithokhoc1414@gmail.com>
2024-12-04 00:12:02 +0700

    Update Address.java

--- src/main/java/com/cosmeticsellingwebsite/entity/Address.java
+++ src/main/java/com/cosmeticsellingwebsite/entity/Address.java
@@ -15,11 +15,16 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
+    @Column(columnDefinition = "text")
     private String receiverName;
     private String receiverPhone;
+    @Column(columnDefinition = "text")
     private String address;
+    @Column(columnDefinition = "text")
     private String province;
+    @Column(columnDefinition = "text")
     private String district;
+    @Column(columnDefinition = "text")
     private String ward;
 
     @ToString.Exclude
