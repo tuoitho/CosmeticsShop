fcc18e01226670eeabc62993555b8543ce7808ce
tuoitho <tuoithokhoc1414@gmail.com>
2024-12-06 11:22:44 +0700

    update voucher

--- src/main/java/com/cosmeticsellingwebsite/service/impl/VoucherService.java
+++ src/main/java/com/cosmeticsellingwebsite/service/impl/VoucherService.java
@@ -1,10 +1,12 @@
 package com.cosmeticsellingwebsite.service.impl;
 
+import com.cosmeticsellingwebsite.dto.VoucherDTO;
 import com.cosmeticsellingwebsite.entity.Voucher;
 import com.cosmeticsellingwebsite.exception.CustomException;
 import com.cosmeticsellingwebsite.repository.VoucherRepository;
 import com.cosmeticsellingwebsite.service.interfaces.IVoucherService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -50,6 +52,21 @@ public class VoucherService implements IVoucherService {
         return voucherRepository.save(voucher);
     }
 
+    public Voucher updateVoucher(Voucher updatedVoucher) {
+        Voucher voucher = voucherRepository.findById(updatedVoucher.getVoucherId())
+                .orElseThrow(() -> new EntityNotFoundException("Không tìm thấy voucher"));
+
+        if (voucher.getUsed()) {
+            throw new IllegalStateException("Không thể chỉnh sửa voucher do mã đang được sử dụng cho đơn hàng hệ thống.");
+        }
+
+        voucher.setVoucherCode(updatedVoucher.getVoucherCode());
+        voucher.setVoucherValue(updatedVoucher.getVoucherValue());
+        voucher.setStartDate(updatedVoucher.getStartDate());
+        voucher.setEndDate(updatedVoucher.getEndDate());
+        return voucherRepository.save(voucher);
+    }
+
     @Override
     public void deleteVoucher(Long id) {
         Voucher voucher = voucherRepository.findById(id)
@@ -76,7 +93,47 @@ public class VoucherService implements IVoucherService {
     @Override
     public Page<Voucher> getVouchersWithPagination(int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by("startDate").descending());
-        return voucherRepository.findAll(pageable);
+//        return voucherRepository.findAll(pageable);
+        return voucherRepository.findDistinctVouchers(pageable);
     }
 
+    public Long countByVoucherCode(String voucherCode) {
+        return voucherRepository.countByVoucherCode(voucherCode);
+    }
+
+    public Long countByUsedTrueAndVoucherCode(String voucherCode) {
+        return voucherRepository.countByUsedTrueAndVoucherCode(voucherCode);
+    }
+
+    public Long countByUsedFalseAndVoucherCode(String voucherCode) {
+        return voucherRepository.countByUsedFalseAndVoucherCode(voucherCode);
+    }
+
+    public void updateVoucher(@Valid VoucherDTO voucher) {
+        Voucher voucherEntity = voucherRepository.findById(voucher.getVoucherId())
+                .orElseThrow(() -> new EntityNotFoundException("Không tìm thấy voucher"));
+
+        //get voucherCode
+        String voucherCode = voucherEntity.getVoucherCode();
+        //lấy chênh lệch giữa số lượng cũ và số lượng mới
+        Long quantity = voucherRepository.countByUsedFalseAndVoucherCode(voucherCode);
+        Long newQuantity = voucher.getQuantityAvailable();
+        long diff = Math.abs(newQuantity - quantity);
+        if (newQuantity < quantity) {
+            for (int i = 0; i < diff; i++) {
+                Voucher voucherToDelete = voucherRepository.findFirstByVoucherCodeAndUsedFalse(voucherCode)
+                        .orElseThrow(() -> new CustomException("Voucher " + voucherCode + " đã được sử dụng"));
+                voucherRepository.delete(voucherToDelete);
+            }
+        }else if (newQuantity > quantity) {
+            for (int i = 0; i < diff; i++) {
+                Voucher newVoucher = new Voucher();
+                newVoucher.setVoucherCode(voucherCode);
+                newVoucher.setVoucherValue(voucher.getVoucherValue());
+                newVoucher.setStartDate(voucher.getStartDate());
+                newVoucher.setEndDate(voucher.getEndDate());
+                voucherRepository.save(newVoucher);
+            }
+        }
+    }
 }
